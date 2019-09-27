20fdbe412f4e807832c221ceeb448df3aa50d631
jingqilong <1969099665@qq.com>
2019-09-27 18:33:25 +0800

    审核流程

--- app/Services/Shop/AuditService.php
+++ app/Services/Shop/AuditService.php
@@ -2,10 +2,26 @@
 namespace App\Services\Shop;
 
 
+use App\Repositories\OaAuditTypeRepository;
 use App\Services\BaseService;
 
 class AuditService extends BaseService
 {
 
+    public function addAudit(array $data)
+    {
+        unset($data['sign'],$data['token']);
+        if (OaAuditTypeRepository::exists(['name' => $data['name']])){
+            return ['code' => 1,'message' => '审核类型名称已存在！'];
+        }
+        if (OaAuditTypeRepository::exists(['name' => $data['url']])){
+            return ['code' => 1,'message' => '审核类型已存在！'];
+        }
+        $data['created_at'] = time();
+        if(!OaAuditTypeRepository::getAddId($data)){
+            return ['code' => 1,'message' => '添加失败,请重试！'];
+        }
+        return ['code' => 200,'message' => '添加成功'];
+    }
 }
             
