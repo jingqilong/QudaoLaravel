cfda2c82603a78395969583a42c907a9a683af73
max <1969099665@qq.com>
2019-11-14 19:39:18 +0800

    Update ProjectService.php

--- app/Services/Project/ProjectService.php
+++ app/Services/Project/ProjectService.php
@@ -29,44 +29,13 @@ class ProjectService extends BaseService
     public function getProjectList()
     {
         $user = $this->auth->user();
-
-        $status= [
-            ProjectEnum::SUBMITTED,
-            ProjectEnum::INREVIEW,
-            ProjectEnum::PASS,
-            ProjectEnum::FAILURE
-        ];
-        $where = ['user_id' => $user['m_id'],'status' => ['in',$status]];
-
+        $where = ['user_id' => $user->m_id,'deleted_at' => 0];
         if (!$list = ProjectOrderRepository::getList($where,['*'],'created_at','desc')){
             $this->setMessage('没有数据！');
             return [];
         }
-
-        foreach ($list as &$value)
-        {
-            switch ($value['status']) {
-                case ProjectEnum::SUBMITTED:
-                    $value['status'] = '已提交';
-                    break;
-                case ProjectEnum::INREVIEW:
-                    $value['status'] = '审核中';
-                    break;
-                case ProjectEnum::PASS:
-                    $value['status'] = '审核通过';
-                    break;
-                case ProjectEnum::FAILURE:
-                    $value['status'] = '审核失败';
-                    break;
-                case ProjectEnum::DELETE:
-                    $value['status'] = '已删除';
-                    break;
-                default ;
-            }
-            $value['reservation_at']    =   date('Y-m-d H:m:s',$value['reservation_at']);
-            $value['created_at']        =   date('Y-m-d H:m:s',$value['created_at']);
-            $value['updated_at']        =   date('Y-m-d H:m:s',$value['updated_at']);
-        }
+        $list['status_name']       =   ProjectEnum::getStatus($list['status']);
+        $list['reservation_at']    =   date('Y-m-d H:m:s',$list['reservation_at']);
         $this->setMessage('查找成功');
         return $list;
     }
@@ -78,15 +47,13 @@ class ProjectService extends BaseService
      */
     public function getProjectInfo(string $id)
     {
-        if (!$list = ProjectOrderRepository::getOne(['id' => $id,'status' => ['in',[1,2,3,4]]])){
-            $this->setError('查询不到该条数据！');
+        $user = $this->auth->user();
+        if (!$list = ProjectOrderRepository::getOne(['id' => $id,'user_id' => $user->m_id],['id','name','mobile','project_name','remark','status'])){
+            $this->setError('暂无数据!');
             return false;
         }
-
+        $list['status_name']       =   ProjectEnum::getStatus($list['status']);
         $list['reservation_at']    =   date('Y-m-d H:m:s',$list['reservation_at']);
-        $list['created_at']        =   date('Y-m-d H:m:s',$list['created_at']);
-        $list['updated_at']        =   date('Y-m-d H:m:s',$list['updated_at']);
-
         $this->setMessage('查找成功');
         return $list;
     }
@@ -160,14 +127,14 @@ class ProjectService extends BaseService
     public function delProject(string $id)
     {
         if (!$ProjectInfo = ProjectOrderRepository::getOne(['id' => $id])){
-            $this->setError('没有查找到该数据,请重试！');
+            $this->setError('暂无数据!');
             return false;
         }
-        if ($ProjectInfo['status'] == ProjectEnum::DELETE){
+        if ($ProjectInfo['deleted'] !== 0){
             $this->setError('项目已被删除！请勿重新操作');
             return false;
         }
-        if (!$res = ProjectOrderRepository::getUpdId(['id' => $id],['status' => ProjectEnum::DELETE])){
+        if (!ProjectOrderRepository::getUpdId(['id' => $id],['deleted' => 0])){
             $this->setError('删除失败！');
             return false;
         }
