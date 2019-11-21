162695945b34c427c74def9ff7dbce6df42174d9
jingqilong <45377915+jingqilong@users.noreply.github.com>
2019-11-21 18:42:32 +0800

    Update DoctorsController.php

--- app/Api/Controllers/V1/Medical/DoctorsController.php
+++ app/Api/Controllers/V1/Medical/DoctorsController.php
@@ -520,7 +520,7 @@ class DoctorsController extends ApiController
      * @OA\Get(
      *     path="/api/v1/medical/get_doctor",
      *     tags={"医疗医院前端"},
-     *     summary="获取医生列表",
+     *     summary="用户获取医生详情",
      *     description="jing" ,
      *     operationId="get_doctor",
      *     @OA\Parameter(
