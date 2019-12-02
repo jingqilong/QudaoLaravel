f9db59fa15bb7f9c00aed4de9a3f0a27edcfa7a6
jingqilong <45377915+jingqilong@users.noreply.github.com>
2019-12-02 17:39:03 +0800

    debug

--- app/Services/Shop/OrderRelateService.php
+++ app/Services/Shop/OrderRelateService.php
@@ -381,13 +381,13 @@ class OrderRelateService extends BaseService
             return $order_list;
         }
         $order_relate_ids      = array_column($order_list['data'],'id');
-        $order_goods_list      = ShopOrderGoodsRepository::getList(['order_relate_id' => ['in',$order_relate_ids]]);
         $express_company_ids   = array_column($order_list['data'],'express_company_id');
         $express_company_list  = CommonExpressRepository::getList(['id' => ['in',$express_company_ids]]);
+        /*$order_goods_list      = ShopOrderGoodsRepository::getList(['order_relate_id' => ['in',$order_relate_ids]]);
         $goods_list            = GoodsSpecRelateService::getListCommonInfo($order_goods_list);
         $goods_ids             = array_column($goods_list,'goods_id');
         $comments              = CommonCommentsRepository::getList(['member_id' => $member->id,'type' => CommentsEnum::SHOP,'related_id' => ['in',$goods_ids]]);
-        $order_relate_ids   = array_column($order_list['data'],'id');
+        */$order_relate_ids   = array_column($order_list['data'],'id');
         $order_goods_list   = ShopOrderGoodsRepository::getList(['order_relate_id' => ['in',$order_relate_ids]]);
         $goods_list         = GoodsSpecRelateService::getListCommonInfo($order_goods_list);
         foreach ($order_list['data'] as &$value){
