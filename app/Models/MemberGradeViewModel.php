6a7b8be3dc69e05498160f0468b914f18d96c4d0
jingqilong <45377915+jingqilong@users.noreply.github.com>
2019-12-17 11:02:26 +0800

    Update MemberGradeViewModel.php

--- app/Models/MemberGradeViewModel.php
+++ app/Models/MemberGradeViewModel.php
@@ -27,7 +27,7 @@ class MemberGradeViewModel extends Model
     protected $primaryKey = 'id';
 
 
-    protected $fillable = ['id','card_no','en_name','image_id','email','end_at','profile','sex','ch_name','mobile','position','address','employer','grade','is_recommend','img_url','title','category','status','hidden','created_at','deleted_at'];
+    protected $fillable = ['id','card_no','en_name','image_id','email','end_at','other_server','profile','sex','ch_name','mobile','position','address','employer','grade','is_recommend','img_url','title','category','status','hidden','created_at','deleted_at'];
 
 
 
