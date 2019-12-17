14e1ceedc530bdb0faafb79cffc90730127ee740
jingqilong <45377915+jingqilong@users.noreply.github.com>
2019-12-17 13:16:59 +0800

    Update OaMemberService.php

--- app/Services/Oa/OaMemberService.php
+++ app/Services/Oa/OaMemberService.php
@@ -93,10 +93,11 @@ class OaMemberService extends BaseService
         $member['status_name']   = MemberEnum::getStatus($member['status'],'成员');
         $member['hidden_name']   = MemberEnum::getHidden($member['hidden'],'显示');
         $member['created_at']    = date('Y-m-d H:i:s',$member['created_at']);
+        if (empty($member['birthday'])) $member['birthday'] = '';
         if (0 == $member['end_at']){
-            $member['end_at']        = MemberEnum::getExpiration(MemberEnum::PERMANENT,'永久有效');
+            $member['end_at']    = MemberEnum::getExpiration(MemberEnum::PERMANENT,'永久有效');
         }else{
-            $member['end_at']        = date('Y-m-d H:i:s',$member['end_at']);
+            $member['end_at']    = date('Y-m-d H:i:s',$member['end_at']);
         }
         $this->setMessage('获取用户信息成功');
         return $member;
@@ -253,7 +254,7 @@ class OaMemberService extends BaseService
         ];
         $info_arr = [
             'member_id'      => $request['id'],
-            'birthday'       => $request['birthday'] ?? '',
+            'birthday'       => $request['birthday'] ?? 0,
             'address'        => $request['address'] ?? '' ,
             'info_provider'  => $request['info_provider'] ?? '',
             'employer'       => $request['employer'] ?? '',
