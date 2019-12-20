97aceff27b3ad38b7ad69780075ba28d30b4fd0e
gaolu1 <184639693@qq.com>
2019-12-20 18:16:39 +0800

    事件

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Events\SendDingTalkEmail;
+use App\Events\SendWeChatPush;
+use App\Events\SendSiteMessage;
+use App\Events\SendFlowSms;
+use App\Listener\ProcessEventSubscriber;
+
+
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +24,22 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        SendDingTalkEmail::class => [
+            //钉钉邮件通知 流程订阅都监听
+            ProcessEventSubscriber::class,
+        ],
+        SendWeChatPush::class => [
+            //微信推送 流程订阅都监听
+            ProcessEventSubscriber::class,
+        ],
+        SendSiteMessage::class => [
+            //站内信息 流程订阅都监听
+            ProcessEventSubscriber::class,
+        ],
+        SendFlowSms::class => [
+            //短信 流程订阅都监听
+            ProcessEventSubscriber::class,
         ],
     ];
 
