33d6da9655d2687354cf2b7c31c1421a2ecc5696
jingqilong <45377915+jingqilong@users.noreply.github.com>
2019-12-23 20:00:09 +0800

    成员按条件查找排序(优化)， 成员查看成员信息(优化)

--- app/Repositories/OaGradeViewRepository.php
+++ app/Repositories/OaGradeViewRepository.php
@@ -19,4 +19,18 @@ class OaGradeViewRepository extends ApiRepository
     {
         $this->model = $model;
     }
+
+    /**
+     * 获取成员可查看级别
+     * @param null $where
+     * @param array $column
+     * @return array|bool
+     */
+    protected function showGrade($where = null, $column = ['*'])
+    {
+        if (!$grade = $this->getList($where,$column)){
+            return false;
+        }
+        return array_column($grade,'value');
+    }
 }
