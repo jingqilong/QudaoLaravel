fcf9104d21f4d46ee9aa7c3c6a8f1f6784d1967c
koko <50900490+SangBoy@users.noreply.github.com>
2019-12-24 17:06:53 +0800

    修改活动收藏列表

--- app/Services/Activity/CollectService.php
+++ app/Services/Activity/CollectService.php
@@ -2,8 +2,10 @@
 namespace App\Services\Activity;
 
 
+use App\Enums\CollectTypeEnum;
 use App\Repositories\ActivityCollectRepository;
 use App\Repositories\ActivityDetailRepository;
+use App\Repositories\MemberCollectRepository;
 use App\Services\BaseService;
 use App\Traits\HelpTrait;
 use Illuminate\Support\Facades\Auth;
@@ -75,7 +77,7 @@ class CollectService extends BaseService
         $page_num   = $request['page_num'] ?? 20;
         $type       = $request['type'];
         $member     = $this->auth->user();
-        $where      = ['member_id' => $member->id,'deleted_at' => 0];
+        $where      = ['type' => CollectTypeEnum::ACTIVITY,'member_id' => $member->id,'deleted_at' => 0];
         $activity_where = ['created_at' => ['<>',0]];
         $time = time();
         switch ($type){
@@ -92,11 +94,11 @@ class CollectService extends BaseService
                 $activity_where['end_time']   = ['<',$time];
                 break;
         }
-        if (!$list = ActivityCollectRepository::getList($where)){
+        if (!$list = MemberCollectRepository::getList($where)){
             $this->setMessage('暂无数据！');
             return ['current_page' => $page,'data' => [],'last_page' => $page,'per_page' => $page_num,'total' => 0];
         }
-        $activity_ids = array_column($list,'activity_id');
+        $activity_ids = array_column($list,'target_id');
         $activity_column = ['id','name','address','price','start_time','end_time','cover_id','theme_id'];
         if (!$activities = ActivityDetailRepository::getActivityList(array_merge($activity_where,['id' => ['in',$activity_ids]]),$activity_column,'start_time','desc',$page,$page_num)){
             $this->setError('获取失败！');
