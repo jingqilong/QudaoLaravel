79b3417e035b79df91f106070edc384a24e3cc3d
gaolu1 <184639693@qq.com>
2019-12-24 14:46:05 +0800

    流程相关人模块

--- config/process.php
+++ config/process.php
@@ -2,18 +2,16 @@
 
 return [
     'getway' =>[
-//        'MEMBER_REGISTER'        => '成员注册',
-            1 => "\\App\\Services\\Member\\GradeOrdersService.NotFound",
 //        'MEMBER_UPGRADE'         => '成员升级',
-            2 => "\\App\\Services\\Member\\GradeOrdersService.NotFound",
+            1 => "\\App\\Services\\Member\\GradeOrdersService.NotFound",
 //        'ACTIVITY_REGISTER'      => '活动报名',
-            3 => "\\App\\Services\\Activity\\RegisterService.NotFound",
+            2 => "\\App\\Services\\Activity\\RegisterService.NotFound",
 //        'PROJECT_DOCKING'        => '项目对接',
-            4 => "\\App\\Services\\Project\\OrderService.NotFound",
+            3 => "\\App\\Services\\Project\\OrderService.NotFound",
 //        'LOAN_RESERVATION'       => '贷款预约',
-            5 => "\\App\\Services\\Loan\\PersonalService.getLoanInfo",
+            4 => "\\App\\Services\\Loan\\PersonalService.getLoanInfo",
 //        'ENTERPRISE_CONSULT'     => '企业咨询',
-            6 => "\\App\\Services\\Enterprise\\OrderService.NotFound",
+            5 => "\\App\\Services\\Enterprise\\OrderService.NotFound",
     ],
     'event_type' => [
         //use App\Events\SendDingTalkEmail;
@@ -25,4 +23,23 @@ return [
         //use App\Events\SendFlowSms;
         4 => 'WECHAT_PUSH',
     ],
+    'process_starter' =>[  //获取发起人信息
+//        'MEMBER_UPGRADE'         => '成员升级',
+        1 => [ App\Services\Member\GradeOrdersService::class,'getCreatedUser'],
+//        'ACTIVITY_REGISTER'      => '活动报名',
+        2 => [ App\Services\Activity\RegisterService::class,'getCreatedUser'],
+//        'PROJECT_DOCKING'        => '项目对接',
+        3 => [ App\Services\Project\OrderService::class,'getCreatedUser'],
+//        'LOAN_RESERVATION'       => '贷款预约',
+        4 => [ App\Services\Loan\PersonalService::class,'getCreatedUser'],
+//        'ENTERPRISE_CONSULT'     => '企业咨询',
+        5 => [ App\Services\Enterprise\OrderService::class,'getCreatedUser'],
+//        'HOUSR_RESERVATION'     => '看房预约',
+        6 => [ App\Services\House\ReservationService::class,'getCreatedUser'],
+//        'HOSPITAL_RESERVATION'     => '医疗预约',
+        7 => [ App\Services\Medical\OrdersService::class,'getCreatedUser'],
+//        'PRIME_RESERVATION'     => '医疗预约',
+        8 => [ App\Services\Prime\ReservationService::class,'getCreatedUser'],
+    ],
+
 ];
