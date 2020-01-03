dd18c0301f748ab9b96b58a70810be64d660afce
gaolu1 <184639693@qq.com>
2020-01-03 13:26:54 +0800

    库存SAERVICE修改

--- app/Services/Shop/ShopInventoryService.php
+++ app/Services/Shop/ShopInventoryService.php
@@ -19,17 +19,18 @@ class ShopInventoryService extends BaseService
         $user = Auth::guard('oa_api')->user();
         //余额
         $remain = $this->getCurrentInventory($request['goods_id'],$request['spec_id']);
-        $remain +=  $request['amount'] * $request['change_type'];
+        $remain += $request['amount'] * $request['change_type'];
         //数据
         $new_data = Arr::only($request,['entry_id','goods_id','spec_id','change_type','change_from','amount']);
         $new_data ['remain'] = $remain;
         $new_data ['created_at'] = time();
         $new_data ['created_by'] = (!empty($user))?$user->id:0;
-        if (!ShopInventoryRepository::getAddId(['content' => $request['content'],'created_at' => time()])){
-            $this->setError('添加失败！');
+        if (!ShopInventoryRepository::getAddId($new_data)){
+            Loggy::write('error','库存记录添加失败！',$new_data);
+            $this->setError('库存记录添加失败！');
             return false;
         }
-        $this->setMessage('添加成功！');
+        $this->setMessage('库存记录添加成功！');
         return true;
     }
 
@@ -42,12 +43,12 @@ class ShopInventoryService extends BaseService
     public function getCurrentInventory($goods_id,$sepc_id=0){
         $inventory = ShopInventoryRepository::getList(
             [['goods_id'=>$goods_id], ['spec_id'=>$sepc_id]],
-            ['id','goods_id','spec_id','remain'],
+            ['remain'], //'id','goods_id','spec_id',
             ['id'],
             ['desc'],
             1,
             1);
-        if($inventory){
+        if(isset($inventory['data']['0'])){
             return $inventory['data']['0']['remain'];
         }
         return 0;
