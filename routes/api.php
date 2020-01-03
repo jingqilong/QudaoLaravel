131973e52b94c4a30dc86a4dabb3c452b457f1f2
koko <50900490+SangBoy@users.noreply.github.com>
2020-01-03 14:08:02 +0800

    修改房产租赁预约列表，新增房产租赁预约详情

--- routes/api.php
+++ routes/api.php
@@ -567,6 +567,7 @@ $api->version('v1',function ($api){
                 $api->get('lease_list', 'LeaseController@leaseList')->name('获取房产租赁方式列表');
                 #预约
                 $api->get('all_reservation_list', 'ReservationController@allReservationList')->name('预约列表');
+                $api->get('reservation_detail', 'ReservationController@reservationDetail')->name('预约详情');
                 $api->post('audit_reservation', 'ReservationController@auditReservation')->name('审核预约');
             });
         });
