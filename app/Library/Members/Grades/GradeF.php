ba13e76dddc20315762afa4ef45d8df226870219
gaolu1 <184639693@qq.com>
2020-01-08 13:16:03 +0800

    成员算法修改

--- app/Library/Members/Grades/GradeF.php
+++ app/Library/Members/Grades/GradeF.php
@@ -5,6 +5,13 @@ namespace App\Library\Members\Grades;
 
 use App\Library\Members\Grade;
 
+/**
+ * Class GradeF
+ * @package App\Library\Members\Grades
+ * @desc 会员等级的子类，注意
+ * 假如算法都一样，实现算法的方法就放在父类中。
+ * 如果算法有所不同，则实现算法的在此类中实现。
+ */
 class GradeF extends Grade
 {
 
