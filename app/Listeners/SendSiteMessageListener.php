d02bf20da15cc51e083df105ea2a343e4b9e9fcf
koko <50900490+SangBoy@users.noreply.github.com>
2020-01-14 15:28:38 +0800

    流程事件

--- app/Listeners/SendSiteMessageListener.php
+++ app/Listeners/SendSiteMessageListener.php
@@ -41,7 +41,7 @@ class SendSiteMessageListener implements ShouldQueue
             ];
             $category = MessageEnum::SYSTEMNOTICE;
             $messageTemplate = new MessageTemplate($message_data,$receiver['receiver_iden']);
-            $sendMethod = ($data['receiver_iden'] == ProcessPrincipalsEnum::STARTER) ? 'sendMessageForMember' : 'sendMessageForEmployee';
+            $sendMethod = ($receiver['receiver_iden'] == ProcessPrincipalsEnum::STARTER) ? 'sendMessageForMember' : 'sendMessageForEmployee';
             $result = app(SendService::class)::$sendMethod(
                 $receiver['receiver_id'],
                 $category,
