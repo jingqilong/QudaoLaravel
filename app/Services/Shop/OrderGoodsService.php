0c0cb69f6941eb03d8f6b86194c5bcd29cfb1231
jingqilong <45377915+jingqilong@users.noreply.github.com>
2020-01-20 16:30:33 +0800

    修改OA获取评论详情、添加评论

--- app/Services/Shop/OrderGoodsService.php
+++ app/Services/Shop/OrderGoodsService.php
@@ -100,14 +100,14 @@ class OrderGoodsService extends BaseService
             $this->setError('订单信息不存在！');
             return false;
         }
-        if ($order['status'] == ShopOrderEnum::FINISHED){
+        /*if ($order['status'] == ShopOrderEnum::FINISHED){
             $this->setError('该订单当前状态不能评价！');
             return false;
         }
         if ($order['status'] != ShopOrderEnum::RECEIVED){
             $this->setError('该订单还未签收，无法评论！');
             return false;
-        }
+        }*/
         $this->setMessage('检查通过！');
         return true;
     }
@@ -119,18 +119,13 @@ class OrderGoodsService extends BaseService
      */
     public function commentCallback($relate_id){
         list($order_relate_id) = explode(',',$relate_id);
-        if (!$order_goods_list = ShopOrderGoodsRepository::getAll(['id' => $order_relate_id])){
+        if (!$order_goods_list = ShopOrderGoodsRepository::getAllList(['order_relate_id' => $order_relate_id])){
             $this->setError('订单商品不存在！');
             return false;
         }
         $relate_ids = [];
         foreach ($order_goods_list as $value){
             $relate_ids[] = $order_relate_id . ',' . $value['goods_id'];
-        }
-        $comment_count = CommonCommentsRepository::count(['relate_id' => ['in',$relate_ids]]);
-        if ($comment_count < count($relate_ids)){
-            $this->setMessage('订单商品未全部评论');
-            return true;
         }
         if (!ShopOrderRelateRepository::getUpdId(['id' => $order_relate_id],['status' => ShopOrderEnum::FINISHED,'updated_at' => time()])){
             $this->setError('订单状态更新失败！');
